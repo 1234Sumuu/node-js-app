550d75efcbd4029e71c658bf3ed770315122355b
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2023-02-17 08:29:16 -0800

    node js logger global connect

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-function sayHello(name) {
-    console.log('Hello' + name);
-}
-sayHello('Sumu');
+// function sayHello(name) {
+//     console.log('Hello' + name);
+// }
+// sayHello('Sumu');
+
+var logger = require('./logger');
+
+console.log(logger);
